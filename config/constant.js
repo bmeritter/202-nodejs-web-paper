d0aab39877fe5f8ad491454f0203b930a8f9f192
bmeritter <bme_ritter@foxmail.com>
2017-02-25 15:31:50 +0800

    modify httpCode to  constant

--- config/constant.js
+++ config/constant.js
@@ -1,3 +1,9 @@
-/**
- * Created by ritter on 17-2-23.
- */
+module.exports = {
+  httpCode: {
+    Ok: 200,
+    CREATED: 201,
+    NOT_FOUND: 404,
+    BAD_REQUEST: 400,
+    NO_CONTENT: 204
+  }
+};
